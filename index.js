378c7feb151ab38c8bfa13c966436ec4eb08e336
Siyasia <148126581+Siyasia@users.noreply.github.com>
2024-11-15 23:34:53 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+// Generate or retrieve temporary user ID
+function getTempUserId() {
+  let tempUserId = localStorage.getItem('tempUserId');
+  if (!tempUserId) {
+    tempUserId = generateUUID();
+    localStorage.setItem('tempUserId', tempUserId);
+  }
+  return tempUserId;
+}
+
+// Generate a UUID
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+// Redirect to Create Group Page
+document.getElementById('start-button')?.addEventListener('click', () => {
+  const userId = getTempUserId();
+  console.log(`Temporary User ID: ${userId}`);
+  window.location.href = 'create-group.html';
+});
